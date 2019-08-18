3f178f6e18e7eb38d6cbfff51e3a5b416b1183d3
hola mundo <jgamboa99@uan.edu.co>
2019-08-18 18:04:15 -0500

    hola mundo1
    public class hola {
    
    	public static void main(String[] args)
    	{
    
    		System.out.println("HOLA MUNDO");
    
    	}
    	//JHONY ARTHOR GAMBOA AVILA
    	//CODIGO: 11161618930
    }

---
+++ Hola.java
@@ -0,0 +1,15 @@
+
+
+public class Hola {
+
+	public static void main(String[] args) 
+	{
+		
+
+		System.out.println("HOLA MUNDO");
+		
+	}
+	//JHONY ARTHOR GAMBOA AVILA
+	//CODIGO: 11161618930
+}
+ 
